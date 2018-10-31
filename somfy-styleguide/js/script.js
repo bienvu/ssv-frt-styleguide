f16191555ab301047a5270656423b1df586e3f1e
Bien Vu <tienbienit1992@gmail.com>
2018-10-31 14:30:09 +0700

    SOM: update style

--- somfy-styleguide/js/script.js
+++ somfy-styleguide/js/script.js
@@ -1,25 +1,10 @@
 /*jslint browser: true*/
 /*global $, jQuery, Modernizr, enquire*/
 (function (window, document, $) {
-  var $html = $('html'),
-    mobileOnly = "screen and (max-width:47.9375em)", // 767px.
-    mobileLandscape = "(min-width:30em)", // 480px.
-    tablet = "(min-width:48em)"; // 768px.
-  // Add  functionality here.
-
-  // Table responsive
-  Drupal.behaviors.tableResponsive = {
-    attach: function (context, settings) {
-      var $table = $('table', context);
-      if ($table.length &&
-        !$table.parent().hasClass('table-responsive')) {
-        $table.not($table.find('table')).wrap('<div class="table-responsive"></div>');
-      }
-    }
-  };
-
   $(document).ready( function () {
-   videojs('my-video');
+    $( window ).load(function() {
+      $('body').addClass('loaded');
+       // videojs('my-video');
+    });
   });
-
 }(this, this.document, this.jQuery));
